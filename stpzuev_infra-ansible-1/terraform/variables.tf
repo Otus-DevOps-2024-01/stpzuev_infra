53a39a2f9e1523f650e4e63109ee806e8ab82d0e
Stepan Zuev <54147166+stpzuev@users.noreply.github.com>
2024-04-16 15:49:16 +0500

    hw8, ansible-1
    перенос проекта из своей репы для другой группы студентов

---
+++ stpzuev_infra-ansible-1/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "cloud_id" {
+  description = "Cloud ID"
+}
+variable "folder_id" {
+  description = "Folder ID"
+}
+variable "service_account_key_file" {
+  description = "Service account key file"
+}
+variable "zone" {
+  description = "Zone"
+  default = "ru-central1-a"
+}
+variable "bucket_name" {
+  description = "Name of S3 bucket"
+}
+variable "name" {
+  description = "SA User Name"
+}
